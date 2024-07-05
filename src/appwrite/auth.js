4f9a5e44c1658f6ce71102837447f79ba9d7f011
userkamaljha <userkamaljha@gmail.com>
2024-07-05 15:30:18 +0530

    fix bug in login method

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -44,6 +44,10 @@ export class AuthService {
 
   async login({ email, password }) {
     try {
+      // Delete the current active session
+    await this.account.deleteSessions();
+    // Wait a moment to ensure the session deletion is fully processed
+    await new Promise(resolve => setTimeout(resolve, 1000));
       return await this.account.createEmailSession(email, password);
     } catch (error) {
       console.log("Error while login Account::", error);
